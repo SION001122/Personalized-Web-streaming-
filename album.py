08f0f3b1ea3cfcbaf158d586f864e7e10d0e6cce
SION001122 <163120715+SION001122@users.noreply.github.com>
2024-12-10 13:23:13 +0900

    Add files via upload

---
+++ album.py
@@ -0,0 +1,59 @@
+from unit import extract_audio_files
+from mutagen import File
+import os
+import json
+def audio_list(file_path):
+        absolute_file_path = []
+        with open(file_path, 'r', encoding='utf-8') as f:
+            for line in f:
+                if "*file*" in line:
+                    file_path = line.split("*file*")[1]
+                    file_path = file_path.strip()
+                    absolute_file_path.append(os.path.abspath(file_path))
+        return absolute_file_path
+
+def audio_album(file_path):
+    audio_album_list = []
+    for line in audio_list(file_path):
+        try:
+            audio = File(line)
+            if 'TALB' in audio.tags:
+               audio_album_list.append(audio.tags['TALB'].text[0])
+               continue
+            if 'ALBUM' in audio:
+                audio_album_list.append(audio.tags['ALBUM'][0])
+                continue
+            if '©alb' in audio:
+                audio_album_list.append(audio.tags['©alb'][0])
+                continue
+            if 'IARL' in audio:
+                audio_album_list.append(audio.tags['IARL'][0])
+                continue
+            
+        except:
+            audio_album_list.append("Unknown")
+            continue
+    return audio_album_list
+
+def album_list(file_list_path):
+    name_list = extract_audio_files(file_list_path)
+    # name과 path를 각각 추출해서 튜플 형태로 저장
+    name_list = [(item['name'], item['path']) for item in name_list]
+    album_dict = dict(zip(name_list, audio_album(file_list_path)))
+    merged_album_dict = {}
+    for song, album in album_dict.items():
+        if album not in merged_album_dict:
+            merged_album_dict[album] = []  # 앨범이 없으면 리스트 초기화
+        merged_album_dict[album].append(song)
+    return merged_album_dict
+
+def json_album_list(file_list_path):
+        album_json = album_list(file_list_path)
+        print("DEBUG: album_list output =", album_json)
+
+        converted_album_json = {
+            album: [{'name': song[0], 'path': song[1]} for song in songs]
+            for album, songs in album_json.items()
+        }
+        # UTF-8로 강제 설정
+        return json.dumps(converted_album_json, ensure_ascii=False, indent=4).encode('utf-8').decode('utf-8')
---
+++ unit.py
@@ -0,0 +1,89 @@
+import os
+import threading
+import time
+from mutagen import File
+from PIL import Image
+import io
+# 오디오 파일 목록을 추출하는 함수
+def extract_audio_files(file_list_path):
+    audio_files = []
+    with open(file_list_path, "r", encoding="utf-8") as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.strip()
+            if "*file*" in line:
+                file_path = line.split("*file*")[1]
+                absolute_file_path = os.path.abspath(file_path)
+                
+                # UTF-8로 인코딩된 경로를 사용하여 처리
+                encoded_path = absolute_file_path.encode('utf-8').decode('utf-8')
+
+                # 다양한 확장자에 따른 처리
+                if encoded_path.endswith(".flac"):
+                    cleaned_file_name = os.path.basename(encoded_path)[:-5]  # '.flac' 제거
+                elif encoded_path.endswith(".wav"):
+                    cleaned_file_name = os.path.basename(encoded_path)[:-4]  # '.wav' 제거
+                elif encoded_path.endswith(".mp3"):
+                    cleaned_file_name = os.path.basename(encoded_path)[:-4]  # '.mp3' 제거
+                elif encoded_path.endswith(".aiff"):
+                    cleaned_file_name = os.path.basename(encoded_path)[:-5]  # '.aiff' 제거
+                elif encoded_path.endswith(".dsf") or encoded_path.endswith(".dff"):
+                    cleaned_file_name = os.path.basename(encoded_path)[:-4]  # '.dsf' 또는 '.dff' 제거
+                else:
+                    cleaned_file_name = os.path.basename(encoded_path)  # 다른 확장자 또는 확장자 없는 파일은 그대로 유지
+                
+                # 파일 정보를 추가
+                audio_files.append({"path": encoded_path, "name": cleaned_file_name})
+    return audio_files
+
+def is_safe_path(base_path, user_path):
+    """
+    주어진 경로가 허용된 디렉토리 내부에 있는지 확인합니다.
+    """
+    abs_base = os.path.abspath(base_path)
+    abs_path = os.path.abspath(os.path.join(base_path, user_path))
+    return abs_path.startswith(abs_base)
+        
+        # 앨범 커버 추출 함수
+def extract_album_cover(file_path):
+    try:
+        audio = File(file_path)
+        if audio is None:
+            print(f"지원되지 않는 파일 형식: {file_path}")
+            return None
+        
+        if "APIC:" in audio.tags:
+            album_cover = audio.tags["APIC:"].data
+        elif "covr" in audio:
+            album_cover = audio["covr"][0]
+        elif hasattr(audio, "pictures") and len(audio.pictures) > 0:
+            album_cover = audio.pictures[0].data
+        else:
+            print("앨범 커버를 찾을 수 없습니다2.")
+            image = Image.open(r".\none.png")
+            image_byte_array = image.save(io.BytesIO(), format="PNG")
+            image.save(image_byte_array, format="PNG")
+            image_byte_array.seek(0)
+            return image_byte_array
+
+        image = Image.open(io.BytesIO(album_cover))
+        image_byte_array = io.BytesIO()
+        image.save(image_byte_array, format="PNG")
+        image_byte_array.seek(0)
+        return image_byte_array
+
+    except Exception as e:
+        print(f"앨범 커버를 추출할 수 없습니다: {e}")
+        return None
+
+def get_audio_duration(file_path):
+    """
+    주어진 음원 파일의 재생 시간을 초 단위로 반환.
+    """
+    try:
+        audio = File(file_path)
+        if audio and audio.info:
+            return audio.info.length
+    except Exception as e:
+        print(f"오류 발생: {e}")
+    return audio.info.length
