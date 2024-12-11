977101ed5c22cf1b7280c585232dd51c6e2ec6ad
Lee Jonghyun <bytecakelake@gmail.com>
2024-12-12 01:21:16 +0900

    restapi server 업데이트

---
+++ restapi-server/endpoints.py
@@ -0,0 +1,74 @@
+# =============================================================================
+# SPDX 가이드: https://reuse.software/spec/
+# SPDX-FileCopyrightText: © 2024 bytecakelake <creator@bytecake.dev>
+# SPDX-License-Identifier: AGPL-3.0 license
+# =============================================================================
+# ++ 목차 ++
+# 
+# =============================================================================
+# ++ 할일 목록 ++
+# TODO: 버전 1의 모든 엔드포인트 기획 및 구성을 완료하세요.
+# TODO: 아직 미쳐 작성하지 못한 주석들을 작성하세요.
+# TODO: 목차를 완성하세요.
+# TODO: sqlite3 데이터베이스의 데이터 아키텍처를 설계한 후, 할일 목록을 업데이트하세요.
+# =============================================================================
+from pydantic import BaseModel
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import ORJSONResponse
+# =============================================================================
+# TITLE: 버전별 API 라우터 정의
+
+apis: dict[str, APIRouter] = { # 버전별 API 라우터 객체 [버전, API 라우터 인스턴스]
+    "v1": APIRouter(
+        default_response_class=ORJSONResponse
+    ),
+}
+# =============================================================================
+# TITLE: 응답 모델 정의
+#
+class ArtistSimpleInfo(BaseModel):
+    profile_url: str
+    name: str
+#
+class AlbumSimpleInfo(BaseModel):
+    id: int
+    title: str
+    cover_url: str
+#
+class MusicGeneralInfo(BaseModel):
+    id: int
+    title: str
+    hls_endpoint: str
+    artist: ArtistSimpleInfo
+    album: AlbumSimpleInfo
+# =============================================================================
+# TITLE: 엔드포인트 정의
+#
+# NOTE: 무분별한 크롤링을 방지하기 위해 GET 메서드를 지양하고 POST 메서드를 권장합니다.
+#
+## INFO: 음원 정보 조회 엔드포인트
+
+@apis["v1"].post("/music/{id}/info", response_model=MusicGeneralInfo, tags=["music", "info"])
+async def music_info_query(id: int) -> dict[str, any]:
+    """
+    ## QUERY: 데이터베이스에서 기본적인 음원정보를 가져옵니다.
+    
+    ### Request
+    - path-parameter
+        - `id`: 음원 ID
+    
+    ### Response
+    
+    - status
+        - `200`: MusicGeneralInfo
+        - `404`: HTTPException{"요청하신 음원을 찾을 수 없습니다."}
+        - `500`: HTTPException{"오류가 발생하여 요청을 처리할 수 없습니다."}
+    
+    """
+    
+    # TODO: #14 음원 정보를 가져오는 코드를 작성하세요.
+    pass
+
+# =============================================================================
+# EOC: endpoints.py
+# =============================================================================
---
+++ restapi-server/serviceapp.py
@@ -0,0 +1,52 @@
+# =============================================================================
+# SPDX 가이드: https://reuse.software/spec/
+# SPDX-FileCopyrightText: © 2024 bytecakelake <creator@bytecake.dev>
+# SPDX-License-Identifier: AGPL-3.0 license
+# =============================================================================
+# ++ 목차 ++
+# 
+# =============================================================================
+# ++ 할일 목록 ++
+# TODO: 목차를 완성하세요.
+# =============================================================================
+# TITLE: 모듈 실행 방지
+# DESCRIPTION: 모듈을 커맨드 라인에서 실행하는것을 방지합니다.
+if __name__ == "__main__": raise "이 스크립트는 직접 실행할 수 없습니다."
+# =============================================================================
+# TITLE: 모듈 불러오기
+# DESCRIPTION: FastAPI 모듈을 불러옵니다.
+from fastapi import FastAPI 
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+# =============================================================================
+# TITLE: 서브패키지 불러오기
+# DESCRIPTION: 이 앱의 서브패키지을 불러옵니다.
+import endpoints
+# =============================================================================
+# TITLE: 서버 인스턴스 생성
+# DESCRIPTION: FastAPI 서버 인스턴스를 생성합니다.
+server = FastAPI(
+    # INFO: API 제목
+    title="Personal Music Streaming Service API",
+    # INFO: API 설명
+    description="This is a RESTful API for a personal music streaming service.",
+    # INFO: 최신 API 버전 
+    version="1.0.0"
+)
+# =============================================================================
+# TITLE: 미들웨어 등록
+# DESCRIPTION: 서버 인스턴스에 미들웨어를 추가합니다.
+# TMI: 자동으로 비보안 HTTP 요청을 HTTPS로 변경합니다.
+server.add_middleware(HTTPSRedirectMiddleware)
+# TMI: gzip 압축알고리즘을 사용하여 트래픽을 최적화합니다. 또한 작은 크기(1KB)의 응답은 압축하면 오버헤드가 발생할수 있으므로 압축하지 않습니다.
+server.add_middleware(GZipMiddleware, minimum_size=1024, compresslevel=9)
+# =============================================================================
+# TITLE: 엔드포인트 등록
+# DESCRIPTION: 모든 엔드포인트를 순차적으로 등록합니다.
+# TMI: API 버전별 엔드포인트를 등록합니다.
+for api_version_tag, api_router in endpoints.apis.items():
+    # INFO: API 버전에 따라 엔드포인트 기본경로는 /api/{버전} 형식으로 지정됩니다.
+    server.include_router(api_router, prefix="/api/%s" % api_version_tag)
+# =============================================================================
+# EOC: serviceapp.py
+# =============================================================================
