a1d0f1fda6285df7dbfe75411718136d74e436d5
SION001122 <163120715+SION001122@users.noreply.github.com>
2025-01-18 23:00:59 +0900

    pywin32 추가

--- requirements.bat
+++ requirements.bat
@@ -1,8 +1,8 @@
 @echo off
 
 echo Installing dependencies...
-pip uninstall -y Flask mutagen Pillow flask-compress
-pip install Flask==2.3.3 mutagen==1.46.0 Pillow==10.1.0 flask-compress==1.14 werkzeug==2.3.7
+pip uninstall -y Flask mutagen Pillow flask-compress pywin32
+pip install Flask==2.3.3 mutagen==1.46.0 Pillow==10.1.0 flask-compress==1.14 werkzeug==2.3.7 pywin32==305
 
 echo Installation completed!
 pause
