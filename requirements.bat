55558b8d53b0dc0b5e7d342384efba54b9aec6e0
SION001122 <163120715+SION001122@users.noreply.github.com>
2025-01-18 21:17:00 +0900

    Update requirements.bat

--- requirements.bat
+++ requirements.bat
@@ -1,6 +1,7 @@
 @echo off
 
 echo Installing dependencies...
+pip uninstall -y Flask mutagen Pillow flask-compress
 pip install Flask==2.3.3 mutagen==1.46.0 Pillow==10.1.0 flask-compress==1.14 werkzeug==2.3.7
 
 echo Installation completed!
