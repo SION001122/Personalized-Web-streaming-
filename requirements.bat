6a1e87b846750972242fd3ad7190d15b50b8e61e
SION001122 <163120715+SION001122@users.noreply.github.com>
2025-01-18 18:51:17 +0900

    자동설치

--- requirements.bat
+++ requirements.bat
@@ -1,4 +1,8 @@
-Flask==2.2.5
-mutagen==1.46.0
-Pillow==10.0.0
-flask-compress==1.13
+@echo off
+
+echo Installing dependencies...
+pip install --upgrade pip
+pip install Flask==2.2.5 mutagen==1.46.0 Pillow==10.0.0 flask-compress==1.13
+
+echo Installation completed!
+pause
