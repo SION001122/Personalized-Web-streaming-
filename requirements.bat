01c36c312b429f5058dfe1763b5770f45b58c1d2
SION001122 <163120715+SION001122@users.noreply.github.com>
2025-01-18 21:12:01 +0900

    호환성 수정

--- requirements.bat
+++ requirements.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Installing dependencies...
-pip install Flask==2.2.1 mutagen==1.46.0 Pillow==10.0.0 flask-compress==1.13
+pip install Flask==2.3.3 mutagen==1.46.0 Pillow==10.1.0 flask-compress==1.14 werkzeug==2.3.7
 
 echo Installation completed!
 pause
